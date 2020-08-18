86b85b41bf22c70b3e43e70a7afe860915005e70
erkand <erkan.d@live.se>
2020-08-18 15:47:57 +0200

    add vpc

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,4 @@
-//--------------------------------------------------------------------
-// Variables
-variable "vpc_default_security_group_egress" {}
-variable "vpc_default_security_group_ingress" {}
-variable "vpc_flow_log_cloudwatch_log_group_kms_key_id" {}
-variable "vpc_vpn_gateway_az" {}
-
-//--------------------------------------------------------------------
-// Modules
-module "vpc" {
-  source  = "app.terraform.io/mokayz-training/vpc/aws"
+module "vpc_example_complete-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/complete-vpc"
   version = "2.48.0"
-
-  database_subnet_assign_ipv6_address_on_creation = "false"
-  default_security_group_egress = "${var.vpc_default_security_group_egress}"
-  default_security_group_ingress = "${var.vpc_default_security_group_ingress}"
-  elasticache_subnet_assign_ipv6_address_on_creation = "false"
-  enable_classiclink = "false"
-  enable_classiclink_dns_support = "false"
-  flow_log_cloudwatch_log_group_kms_key_id = "${var.vpc_flow_log_cloudwatch_log_group_kms_key_id}"
-  flow_log_cloudwatch_log_group_retention_in_days = 10
-  flow_log_log_format = "txt"
-  intra_subnet_assign_ipv6_address_on_creation = "false"
-  private_subnet_assign_ipv6_address_on_creation = "false"
-  public_subnet_assign_ipv6_address_on_creation = "false"
-  redshift_subnet_assign_ipv6_address_on_creation = "false"
-  vpn_gateway_az = "${var.vpc_vpn_gateway_az}"
 }
